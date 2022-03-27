d5e83c4f05e8bbd8fcc9b77bc6f30442a9c553d6
oskilochka <yulia.osk14@gmail.com>
2022-03-28 00:22:15 +0300

    change logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Main from "./Main";
 import { BrowserRouter } from "react-router-dom";
+import Main from "./Main";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
