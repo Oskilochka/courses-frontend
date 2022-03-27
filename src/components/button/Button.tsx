e94ffda49f5bf33622ab162dc22fb453258f56b2
oskilochka <yulia.osk14@gmail.com>
2022-03-27 23:07:02 +0300

    create general base for button and input

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./styles.module.scss";
+
+// TODO: trigger on action
+
+type ButtonVariant = "contained" | "outlined" | "text"
+type ButtonType = "error" | "submit" | "text"
+
+interface InputProps {
+  id?: string
+  label?: string,
+  onChange?: (e: React.ChangeEvent<HTMLButtonElement>) => void,
+
+  variant?: ButtonVariant,
+  type?: ButtonType,
+
+  disabled?: boolean,
+  className?: string,
+
+  icon?: React.ReactNode
+}
+
+// hover, disabled, focused, pressed, animation ?
+
+export const Input = React.memo<InputProps>((
+  {
+    id,
+    label,
+    onChange,
+
+    disabled,
+    className,
+
+    variant,
+    type,
+
+    icon,
+  },
+) => {
+  return (
+    <div className={classnames(styles.root, className)}>
+      <button
+        id={id}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {!!icon ? icon : null}
+        {label}
+      </button>
+    </div>
+  );
+});
+
+export default Input;
---
+++ src/components/fields/Input/Input.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./styles.module.scss";
+
+// TODO: remake later to a general component
+// for textarea, textField, timeField, NumberField, PasswordField etc
+
+export interface InputProps {
+  id?: string
+  label?: string,
+  value: any,
+  onChange?: (e?: React.ChangeEvent<HTMLInputElement>) => void,
+  placeholder?: string,
+
+  disabled?: boolean,
+  className?: string,
+
+  error?: boolean,
+  errorText?: string,
+}
+
+export const Input = React.memo<InputProps>((
+  {
+    id,
+    label,
+    value,
+    onChange,
+    placeholder = "Type here...",
+    error,
+    errorText,
+    disabled,
+    className
+  },
+) => {
+  return (
+    <div className={classnames(styles.root, className)}>
+      {label && <label htmlFor={id} title={label} />}
+      <input
+        id={id}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        placeholder={placeholder}
+      />
+      {error && <span>{errorText}</span>}
+    </div>
+  );
+});
+
+export default Input;
